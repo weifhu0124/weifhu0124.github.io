1b91c892349819b231293fb0c827a4e0670e65fc
Weifeng Hu <weifhu@umich.edu>
2017-11-24 00:29:57 -0500

    add education page

---
+++ js/expandable.js
@@ -0,0 +1,18 @@
+var exp = document.getElementsByClassName("exp-li");
+var i;
+
+for (i = 0; i < exp.length; i++) {
+    exp[i].onclick = function(){
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
+}
